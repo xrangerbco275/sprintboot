d668adf29a583615745f5b9ad1901e3f76689bc1
joe cruz <xrangerbco275@att.net>
2019-01-15 16:26:02 -0600

    B-0001
    new project

---
+++ explorecali/src/main/java/com/example/ec/explorecali/ExplorecaliApplication.java
@@ -0,0 +1,14 @@
+package com.example.ec.explorecali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorecaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorecaliApplication.class, args);
+	}
+
+}
+
---
+++ explorecali/src/test/java/com/example/ec/explorecali/ExplorecaliApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.ec.explorecali;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExplorecaliApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
